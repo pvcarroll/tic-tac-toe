c6e2a143320e6925403e00e7ae358d7c9bfc09c8
paulcarroll <pvcarroll@gmail.com>
2016-09-02 14:41:48 -0500

    onclick POST with jquery

---
+++ public/javascripts/client.js
@@ -0,0 +1,17 @@
+var move = function(row, col) {
+        alert("boardClicked: " + row + " " + col);
+
+        var coordinates = {
+            row: row,
+            col: col
+        };
+        $.ajax({
+            type: "POST",
+            data: JSON.stringify(coordinates),
+            contentType: "application/json",
+            success: function(data) {
+                alert("POST SUCCESS")
+                alert(data);
+            }
+        });
+};
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tic-Tac-Toe' });
 });
 
+router.post("/", function(req, res, next) {
+    console.log("/ POST");
+    console.log("req = " + req.body);
+    res.render("index", { title: "Tic-Tac-Toe" });
+});
+
 module.exports = router;

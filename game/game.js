58d0c673cca1415df2afea98b086d4fc78bf5b8c
paulcarroll <pvcarroll@gmail.com>
2016-09-03 12:45:32 -0500

    stop updating after win

--- game/game.js
+++ game/game.js
@@ -1,39 +1,39 @@
-var currentRound = 0;
-var pieces = {
-    x: "X",
-    o: "O"
-};
-var board = [["", "", ""], ["", "", ""], ["", "", ""]];
+var gameState = {
+    currentRound: 0,
+    gameOver: false,
+    pieces: { x: "X", o: "O"},
+    board: [["", "", ""], ["", "", ""], ["", "", ""]]
+}
 
 function checkForWinner() {
     var match = false;
     var rowMatch, columnMatch;
     var diagonalMatch = reverseDiagonalMatch = true;
     var rowValue, columnValue;
-    var diagonalValue = board[0][0];
-    var reverseDiagonalValue = board[0][board.length - 1];
+    var diagonalValue = gameState.board[0][0];
+    var reverseDiagonalValue = gameState.board[0][gameState.board.length - 1];
 
-    for (var i = 1; i < board.length; i++) {
-        if (!diagonalValue || (board[i][i] !== diagonalValue)) {
+    for (var i = 1; i < gameState.board.length; i++) {
+        if (!diagonalValue || (gameState.board[i][i] !== diagonalValue)) {
             diagonalMatch = false;
         }
-        if (!reverseDiagonalValue || (board[i][board.length - i - 1] !== reverseDiagonalValue)) {
+        if (!reverseDiagonalValue || (gameState.board[i][gameState.board.length - i - 1] !== reverseDiagonalValue)) {
             reverseDiagonalMatch = false;
         }
     }
     if (diagonalMatch || reverseDiagonalMatch) {
         return true;
     }
-    for (var i = 0; i < board.length; i++) {
+    for (var i = 0; i < gameState.board.length; i++) {
         rowMatch = true;
         columnMatch = true;
-        rowValue = board[i][0];
-        columnValue = board[0][i];
-        for (var j= 1; j < board.length; j++) {
-            if (!rowValue || (board[i][j] !== rowValue)) {
+        rowValue = gameState.board[i][0];
+        columnValue = gameState.board[0][i];
+        for (var j= 1; j < gameState.board.length; j++) {
+            if (!rowValue || (gameState.board[i][j] !== rowValue)) {
                 rowMatch = false;
             }
-            if (!columnValue || (board[j][i] !== columnValue)) {
+            if (!columnValue || (gameState.board[j][i] !== columnValue)) {
                 columnMatch = false;
             }
         }
@@ -46,17 +46,21 @@ function checkForWinner() {
 
 module.exports = {
     processMove: function(row, col) {
+        if (gameState.gameOver) {
+            return;
+        }
         var moveData = {};
-        var piece = ((currentRound % 2 === 0) ? pieces.x : pieces.o);
+        var piece = ((gameState.currentRound % 2 === 0) ? gameState.pieces.x : gameState.pieces.o);
         moveData.piece = piece;
-        board[row][col] = piece;
+        gameState.board[row][col] = piece;
         moveData.winner = checkForWinner();
-        if (!moveData.winner && (currentRound >= board.length * board.length - 1)) {
+        if (!moveData.winner && (gameState.currentRound >= gameState.board.length * gameState.board.length - 1)) {
             moveData.tie = true;
         }
-        console.log("round = " + currentRound);
-        console.log("board: %j", board);
-        currentRound++;
+        if (moveData.winner || moveData.tie) {
+            gameState.gameOver = true;
+        }
+        gameState.currentRound++;
         return moveData;
     }
 };
